291fa04a0066e6fb68284a4bce0698470f7dbdcc
najafhanif04 <najafhanif29@gmail.com>
2024-11-27 01:58:38 +0500

    adding swipper slider

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,25 @@
-import React from "react";
-import Image from "../assets/images/ugg-671990.png";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Slider from "../assets/images/ugg-671990.png"
 
-export default () => (
-    <div>
-      <img alt="" src={Image} className="w-full h-screen object-cover" />
-    </div>
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 
+// import required modules
+import { Navigation } from 'swiper/modules';
 
+export default function App() {
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide><img src={Slider}></img></SwiperSlide>
+        <SwiperSlide><img src={Slider}></img></SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
 
-);
+      </Swiper>
+    </>
+  );
+}
